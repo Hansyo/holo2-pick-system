91854812fac595d371b09f69563e7c431fc73ef3
Hansyo <hansyo.souti.1999@gmail.com>
2021-06-08 00:13:31 +0900

    [Update] 引継ぎ前と同等レベルになった
    以下の内容が変更点である
    1. websocketの使い方について理解し、実装を行った。
    2. 音声コマンドを増やし、ちょびっとだけ柔軟なシステムにした。
    3. アプリケーション終了時に、自動的にサーバーとのコネクションが落ちるようにした。
    4. 引継ぎ前に追加で、サーバとの切断/再接続ができるようになった。
    5. 不要なオブジェクトやスクリプトを削除した。
    6. 不要なMRTKの要素を使用しないように調整した。

--- Assets/scripts/ConnectToServer.cs
+++ Assets/scripts/ConnectToServer.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using TMPro;
+
+//#define WINDOWS_UWP // コーディングの際にシンタックスハイライトをつけたいため。保存する際はコメントアウトすること
+
 #if WINDOWS_UWP
 // Web Socket https://docs.microsoft.com/ja-jp/windows/uwp/networking/websockets
 using Windows.Networking.Sockets;
@@ -17,18 +21,13 @@ public class ConnectToServer : MonoBehaviour
     private MessageWebSocket websocket;
     private string ipaddr = "192.168.11.4";
 #endif
+    public TextMeshProUGUI recvText;
 
     // Start is called before the first frame update
     void Start()
     {
-#if WINDOWS_UWP
-        websocket = new MessageWebSocket();
-        websocket.Control.MessageType = SocketMessageType.Utf8;
-        websocket.MessageReceived += WebSocket_MessageReceived;
-        websocket.Closed += WebSocket_Closed;
-
-        OnConnect(new Uri("ws:" + ipaddr + ":10090/w"));
-#endif
+        // アプリケーション起動時に必ずサーバーとの接続処理を入れる
+        OnConnect();
     }
 
     // Update is called once per frame
@@ -38,14 +37,25 @@ public class ConnectToServer : MonoBehaviour
     }
 
 #if WINDOWS_UWP
-    void OnConnect(Uri uri)
+
+    // サーバーと接続する。すでに接続しているなら、何もしない
+    public void OnConnect()
     {
+        if (websocket != null) return;
+
+        // 本来なら変数はStartで初期化したいが、後々の事を考えて、こちらで初期化している。
+        // つまり、コネクションを再接続する可能性があることを意味している。
+        websocket = new MessageWebSocket();
+        websocket.Control.MessageType = SocketMessageType.Utf8;
+        websocket.MessageReceived += WebSocket_MessageReceived;
+        websocket.Closed += WebSocket_Closed;
+
         Debug.Log("OnConnect");
         try {
             Task.Run(async () =>
             {
-                await websocket.ConnectAsync(uri);
-                await WebSocket_SendMessage(websocket, "Holo:Connect");
+                await websocket.ConnectAsync(new Uri("ws://" + ipaddr + ":10090/w"));
+                await WebSocket_SendMessage("echo: Holo:Connect");
             });
         } catch (Exception e)
         {
@@ -53,12 +63,24 @@ public class ConnectToServer : MonoBehaviour
         }
     }
 
-    // ただのwrapper
-    void SendMessage(string s)
+    // メッセージを受け取った際に表示データを更新する関数。
+    private void RecvMessage(string s)
+    {
+        UnityEngine.WSA.Application.InvokeOnAppThread(() =>
+        {
+            this.recvText.SetText(s);
+            Invoke("ClearText", 5.0f); // 5sec後にClearTextを呼び出す。
+        }, false);
+    }
+
+    // Unity側で認識させるためのラッパー
+    public void SendToServer(string s)
     {
         try
         {
-            WebSocket_SendMessage(websocket, s);
+            Task.Run(async () => {
+                await WebSocket_SendMessage(s);
+            });
         }
         catch (Exception e)
         {
@@ -66,33 +88,102 @@ public class ConnectToServer : MonoBehaviour
         }
     }
 
-    private async Task WebSocket_SendMessage(MessageWebSocket webSock, string message)
+    // 追加情報欄を空にする
+    public void ClearText()
     {
-        DataWriter mWriter = new DataWriter(webSock.OutputStream);
-        mWriter.WriteString(message);
-        await mWriter.StoreAsync();
-        mWriter.DetachStream(); // ストリームの破棄らしいが、意味がよくわからん。メッセージ送ったらきちんとストリーム切ろうねってことかも
-    }
-
-    private void WebSocket_MessageReceived(MessageWebSocket webSock, MessageWebSocketMessageReceivedEventArgs args)
-    {
-        DataReader mReader = args.GetDataReader();
-        mReader.UnicodeEncoding = UnicodeEncoding.Utf8;
-        string mString = mReader.ReadString(mReader.UnconsumedBufferLength);
-
-        Task.Run(async () =>
+        UnityEngine.WSA.Application.InvokeOnAppThread(() =>
         {
-            await WebSocket_SendMessage(webSock, "Holo:" + mString); // serverにログを残す
-            // Do Something; ex) Debug.Log;
-            Debug.Log("Received: " + mString);
-            Task.Delay(100);
-        });
+            this.recvText.SetText("");
+        }, false);
     }
 
+    // 実際にサーバーに送信を行う。外部からは呼び出されない
+    private async Task WebSocket_SendMessage(string message)
+    {
+        using (var dataWriter = new DataWriter(this.websocket.OutputStream))
+        {
+            dataWriter.WriteString(message);
+            await dataWriter.StoreAsync();
+            dataWriter.DetachStream();
+        }
+    }
+
+    // メッセージを受信した際に自動的に呼び出される。
+    // ハンドラのため、必要のないsenderを受け取っている。
+    // senderを使えばいろいろできるのだろうか...?
+    private void WebSocket_MessageReceived(MessageWebSocket sender, MessageWebSocketMessageReceivedEventArgs args)
+    {
+        try
+        {
+            using (DataReader dataReader = args.GetDataReader())
+            {
+                // DataReaderを用いて受信したデータを解読
+                dataReader.UnicodeEncoding = UnicodeEncoding.Utf8;
+                string message = dataReader.ReadString(dataReader.UnconsumedBufferLength);
+
+                // 表示データを更新
+                Debug.Log("Received: " + message);
+                RecvMessage(message);
+
+                Task.Run(async () =>
+                {
+                    // serverにログを残す
+                    await WebSocket_SendMessage("Holo:" + message);
+                    // Do Something; ex) Debug.Log();
+                    //Task.Delay(100);
+                });
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Error: " + e.ToString());
+        }
+
+    }
+
+    // 実は、公式ページの丸パクリだったり...
     private void WebSocket_Closed(IWebSocket webSock, WebSocketClosedEventArgs args)
     {
-        // WebSock_Closed
-        //webSock.Close();
+        Debug.Log("WebSocket_Closed; Code: " + args.Code + ", Reason: \"" + args.Reason + "\"");
+    }
+
+    // サーバーとの接続を切る。これをやらないと、毎回不正に切られてしまう。それはよくない
+    public void CloseConnection()
+    {
+        if (websocket == null) return;
+        this.websocket.Dispose(); // 接続を切る
+        websocket = null; // nullを入れることで、接続の有無を明確に
+    }
+
+    // 音声コマンドでアプリケーションを終了させる
+    public void FinishApplication()
+    {
+        if (websocket != null) CloseConnection(); // 接続が切れてないなら切る
+        Application.Quit();
+    }
+
+#else
+    public void OnConnect()
+    {
+        this.recvText.SetText("Start Connection");
+    }
+    public void CloseConnection()
+    {
+        this.recvText.SetText("Close Connection");
+    }
+    public void SendToServer(string s)
+    {
+        this.recvText.SetText("send ->" + s);
+    }
+    public void ClearText()
+    {
+        this.recvText.SetText("");
+    }
+
+    public void FinishApplication()
+    {
+        this.recvText.SetText("Finish Application.");
+        Application.Quit();
     }
 #endif
 }
