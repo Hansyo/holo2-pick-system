8934c4676d0efa99d219111cd3906f1c55e96fcf
linchery <linda@ucl.nuee.nagoya-u.ac.jp>
2021-08-08 02:35:05 +0900

    [Add] Navigation System

---
+++ Assets/scripts/TargetMoveManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Input;
+
+public class TargetMoveManager : MonoBehaviour
+{
+    public float timeOut;
+
+    private int i = 0;
+
+    private Vector3[] targetPosition = {
+        new Vector3(2f, 0f, 2f),
+        new Vector3(-2f, 0f, -2f),
+        new Vector3(-2f, 0f, 2f),
+        new Vector3(2f, 0f, -2f),
+    };
+
+
+    void Start() {
+        StartCoroutine( FuncCoroutine() );
+    }
+
+    IEnumerator FuncCoroutine() {
+        int i = 0;
+        while(true){
+            TargetPositionUpdate();
+            yield return new WaitForSeconds(timeOut);
+        }
+    }
+
+    public void TargetPositionUpdate()
+    {
+        if (i < targetPosition.Length)
+        {
+            this.gameObject.transform.position = targetPosition[i];
+            i++;
+        }
+        else
+        {
+            i = 0;
+            this.gameObject.transform.position = targetPosition[i];
+            i++;
+        }
+
+    }
+}
